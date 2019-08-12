f162d4965e38ca93ab2cab524964f00c35cf3044
yanduanyin <1097720997@qq.com>
2019-08-12 10:22:47 +0800

    我的第一二个小程序

---
+++ shopping/pages/user/user.js
@@ -0,0 +1,103 @@
+// pages/user/user.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  payOrders() {
+    wx.requestPayment({//调取支付
+      timeStamp: 'String1',
+      nonceStr: 'String2',
+      package: 'String3',
+      signType: 'MD5',
+      paySign: 'String4',
+      success: function (res) {
+        console.log(res)
+      },
+      fail: function (res) {
+        wx.showModal({
+          title: '支付提示',
+          content: '<text>',
+          showCancel: false
+        })
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    let self = this
+    wx.request({//请求数据
+      url: 'http://www.gdfengshuo.com/api/wx/orders.txt',
+      success(res) {
+        self.setData({
+          orders: res.data
+        })
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    wx.getStorage({//获取数据
+      key: 'address',//getStorage 将数据存储在本地缓存中指定的 key 中
+      success: (res) => {
+        console.log(res)
+        this.setData({
+          hasAddress: true,
+          address: res.data
+        })
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
